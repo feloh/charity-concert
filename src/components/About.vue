7f864be959a42f15eb140cd47d6e8f948589090a
Felix Lohmann <feloh@gmx.de>
2021-07-29 20:54:58 +0200

    New Structure

---
+++ src/components/About.vue
@@ -0,0 +1,45 @@
+<template>
+<v-card>
+  <v-img
+      max-width="70%"
+      class="mx-auto"
+      :src="description.src"
+  ></v-img>
+  <v-card-text>
+    {{description.text}}
+  </v-card-text>
+  <v-card-title
+      class="d-flex justify-center vollkorn"
+  >
+    Wofür sammeln wir?
+  </v-card-title>
+  <v-card-text>
+    {{donation.text}}
+  </v-card-text>
+  <v-img
+      max-width="70%"
+      class="mx-auto"
+      :src="donation.src"
+  ></v-img>
+</v-card>
+</template>
+
+<script>
+export default {
+name: "About",
+  props:{
+  description:{
+    type: Object,
+    default: () =>{return {src:"", text: "Es ist ein Fehler aufgetreten."}}
+  },
+    donation:{
+      type: Object,
+      default: () => {return  {src:"", text: "Es ist ein Fehler aufgetreten."}}
+    },
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
